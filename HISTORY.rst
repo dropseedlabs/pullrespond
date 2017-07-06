c2cab6f86e86e9d256b8790239d79a5cd04c9548
Dave Gaeddert <dave.gaeddert@gmail.com>
2017-07-05 23:45:44 -0500

    Preparing release 0.2.0

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,7 +2,7 @@
 History
 =======
 
-0.1.3 (unreleased)
+0.2.0 (2017-07-05)
 ------------------
 
 * Add files_changed
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='sweep',
-    version='0.1.3.dev0',
+    version='0.2.0',
     description="A simple CLI for responding to and merging pull requests.",
     long_description=readme + '\n\n' + history,
     author="Dropseed, LLC",
